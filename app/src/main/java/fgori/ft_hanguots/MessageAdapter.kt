042b9304f35febe96608a38ab49a691ae985d7dd
fgori <98gori@gmail.com>
2025-11-25 13:32:26 +0100

    finish login and start mainlist

--- app/src/main/java/fgori/ft_hanguots/MessageAdapter.kt
+++ app/src/main/java/fgori/ft_hanguots/MessageAdapter.kt
@@ -1,24 +1,56 @@
 package fgori.ft_hanguots
 
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
+
+
 class MessageAdapter(private val messageList: List<Message>) : RecyclerView.Adapter<MessageAdapter.MessageViewHolder>() {
-{
+
     class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
         val messageText: TextView = itemView.findViewById(R.id.messageTextView)
     }
 
+    override fun getItemViewType(position: Int): Int {
+        val message = messageList[position]
+        if (message.direction == MsgDir.IN) {
+            return 0
+        }
+        return 1
 
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder{}
-    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {}
-    override fun getItemCount(): Int {}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder{
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.chat_item, parent, false)
+        return MessageViewHolder(itemView)
+    }
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val message = messageList[position]
+        holder.messageText.text = message.content
+        val params = holder.messageText.layoutParams as ConstraintLayout.LayoutParams
+        if (getItemViewType(position) == 0)
+        {
+            holder.messageText.setBackgroundResource(R.drawable.bg_chat_bubble_sent)
+            params.startToEnd = ConstraintLayout.LayoutParams.UNSET
+            params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+        }
+        else
+        {
+            holder.messageText.setBackgroundResource(R.drawable.bg_chat_bubble_received)
+            params.startToEnd = ConstraintLayout.LayoutParams.UNSET
+            params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID
+        }
+        holder.messageText.layoutParams = params
+    }
+    override fun getItemCount(): Int {
+        return messageList.size
+    }
+
 
 }
---
+++ app/src/main/java/fgori/ft_hanguots/chatContactAdapter.kt
@@ -0,0 +1,12 @@
+package fgori.ft_hanguots
+
+import android.widget.ImageView
+import android.widget.TextView
+
+
+class chatContactAdapter (contact: Contact) {
+
+    val image = findViewById<ImageView>(R.id.contactImg)
+
+
+}
